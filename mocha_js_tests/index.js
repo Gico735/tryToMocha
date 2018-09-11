8b3a67dcd227144a5fbdbf5800fc98d0517378aa
Gico735 <gic0@bk.ru>
2018-09-12 00:37:27 +0300

    init

---
+++ mocha_js_tests/index.js
@@ -0,0 +1,8 @@
+// const animatorTest = require('./../vanilla_js_tests/animator.spec').animatorTest
+describe('Anime', (Animator) => {
+  
+})
+
+module.exports = {
+  animatorTest: animatorTest
+} 
